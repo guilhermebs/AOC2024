b4ac88168bc0473553ccaf6a52faaae8fa03372c
Guilherme Saturnino <guilherme.bicalho2@gmail.com>
2024-12-18 11:00:07 -0300

    day18 cleanup

--- day18.py
+++ day18.py
@@ -5,35 +5,26 @@ import networkx as nx
 
 def solve():
     input_file_contents = open(os.path.join("input", "day18")).read().rstrip()
-    SIZE = 71
+    MAX_DIM = 70
     bytes_to_use = 1024
     byte_positions = [
-        tuple(int(p) for p in line.split(","))
-        for line in input_file_contents.splitlines()
+        tuple(map(int, line.split(","))) for line in input_file_contents.splitlines()
     ]
-    G = nx.Graph()
-    G.add_edges_from(
-        ((x, y), (xx, yy))
-        for x in range(SIZE)
-        for y in range(SIZE)
-        for (xx, yy) in ((x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1))
-        if 0 <= xx < SIZE
-        and 0 <= yy < SIZE
-    )
+    G = nx.grid_2d_graph(MAX_DIM + 1, MAX_DIM + 1)
 
     G.remove_nodes_from(byte_positions[:bytes_to_use])
 
-    sol_part1 = nx.shortest_path_length(G, (0, 0), (SIZE - 1, SIZE - 1))
+    sol_part1 = nx.shortest_path_length(G, (0, 0), (MAX_DIM, MAX_DIM))
     print("Part 1:", sol_part1)
 
     i = bytes_to_use
-    while nx.has_path(G, (0, 0), (SIZE - 1, SIZE - 1)):
+    while nx.has_path(G, (0, 0), (MAX_DIM, MAX_DIM)):
         i += 1
         if byte_positions[i] in G:
             G.remove_node(byte_positions[i])
 
     sol_part2 = byte_positions[i]
-    print("Part 2:", sol_part2)
+    print("Part 2:", ",".join(map(str, sol_part2)))
 
 
 if __name__ == "__main__":
